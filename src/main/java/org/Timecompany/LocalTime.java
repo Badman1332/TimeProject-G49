0ae3b99ce371ee9bc39694a6f0e6d254c26680ae
Badman1332 <mik.davidsson@gmail.com>
2024-03-07 15:39:32 +0100

    New time pew

---
+++ src/main/java/org/Timecompany/LocalTime.java
@@ -0,0 +1,11 @@
+package org.Timecompany;
+
+import java.time.LocalDate;
+
+public class LocalTime {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+                System.out.println(today);
+    }
+}
