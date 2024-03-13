f1e9c9cbf1b3c2a610ee05935c0c9298baeb1ba7
Badman1332 <mik.davidsson@gmail.com>
2024-03-13 10:45:26 +0100

    New time pew

--- src/main/java/org/Timecompany/App.java
+++ src/main/java/org/Timecompany/App.java
@@ -2,10 +2,9 @@ package org.Timecompany;
 
 public class App {
     public static void main(String[] args) {
-        // Declare and initialize an integer array
-        int[] numbers = {11, 23, 39, 42 ,56}; // Feel free to add more elements as needed
 
-        // Print the elements of the array
+        int[] numbers = {11, 23, 39, 42 ,56};
+
         for (int num : numbers) {
             System.out.print(num + " ");
         }
