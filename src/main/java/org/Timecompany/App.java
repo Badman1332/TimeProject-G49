ed769bec4e040c4b12d3f1af2e7ef23b1ddcf5aa
Badman1332 <mik.davidsson@gmail.com>
2024-03-13 10:43:46 +0100

    New time pew

--- src/main/java/org/Timecompany/App.java
+++ src/main/java/org/Timecompany/App.java
@@ -1,13 +1,13 @@
 package org.Timecompany;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Declare and initialize an integer array
+        int[] numbers = {11, 23, 39, 42 ,56}; // Feel free to add more elements as needed
+
+        // Print the elements of the array
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
     }
 }
